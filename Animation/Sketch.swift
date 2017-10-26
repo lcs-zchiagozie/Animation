80f346c8afadcbed0a86423df1f77995fdd3b8e6
Chiagozie, Zachary <zchiagozie@lcs-mba3000.lcs.on.ca>
2017-10-26 11:00:07 -0400

    My try on the bouncy ball wall

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    
+    var dx : Int
     // This function runs once
     override init() {
         
@@ -17,18 +17,29 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
+        // set dx (change in x) value
+        dx = 2
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(centreX: 0, centreY: 0, width: 5000, height: 5000
+    )
         
         // Change position
-        x += 1
-        
+        x += dx
+        if x >= 500 {
+            dx = -2
+        }
+        if x <= 0 {
+            dx = 2
+        }
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
     }
     
 }
