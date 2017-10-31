abcd2b2efd4cc21e0bbf5c0f323e5d9585ded63c
Chiagozie, Zachary <zchiagozie@lcs-mba3000.local>
2017-10-31 14:46:29 -0400

    Revised and its good to go!

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,6 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
-    var y : Int
     // This function runs once
     override init() {
         
@@ -17,7 +16,6 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 200
-        y = 400
     }
     
     // Runs in a loop, forever, to create the animated effect
@@ -25,14 +23,22 @@ class Sketch : NSObject {
         
         // Change position
         x += 1
-        y += -1
         // Draw an ellipse in the middle of the canvas
        canvas.fillColor = Color.purple
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 150, width: 50, height: 50)
     
         canvas.drawShapesWithBorders = false
         canvas.fillColor = Color.green
-        canvas.drawEllipse(centreX: y, centreY: 300, width: 50, height: 50)
+        canvas.drawEllipse(centreX: 500-x, centreY: 50, width: 50, height: 50)
+        
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: 500-x, centreY: 350, width: 50, height: 50)
+        
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
     }
     
 
